7b2123600dada857fa3450afe5f839a92ca32151
Ирина Бобина <bobin.ir495@gmail.com>
2020-08-16 00:59:46 +0300

    ипое

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import Post
+from django.forms import ModelForm
 
 
-class PostForm(forms.Form):
-    text = forms.TextField()
-    group = forms.ChoiceField()
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['text', 'group']
