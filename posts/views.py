866d47aba4fc7f98ce8fd89009fb925c229026a5
Ирина Бобина <bobin.ir495@gmail.com>
2020-08-16 12:32:26 +0300

    jnkj

--- posts/views.py
+++ posts/views.py
@@ -20,11 +20,12 @@ def group_posts(request, slug):
 @login_required
 def new_post(request):
     if request.method == 'POST':
+        form = PostForm(request.POST)
         if form.is_valid():
-            form = PostForm(request.POST)
-            Post.group = form.cleaned_data['group'] 
-            Post.text = form.cleaned_data['text']
-            Post.save()
+            new_ one = form.save()
+            new_one.group = form.cleaned_data['group'] 
+            new_one.text = form.cleaned_data['text']
+            form.save()
             return redirect('index')
     form = PostForm()
     return render(request, 'new.html', {'form': form})
