2f1ce2d7fa25b7ef5c1e6909fa9f76bf66df3651
Ирина Бобина <bobin.ir495@gmail.com>
2020-09-05 23:29:13 +0300

    bfg

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
+from django.core.paginator import Paginator
 
 from .models import Post, Group
 from .forms import PostForm
 
 
 def index(request):
-    latest = Post.objects.all()[:11]
-    return render(request, "index.html", {"posts": latest})
+    post_list = Post.objects.order_by('-pub_date').all()
+    paginator = Paginator(post_list, 10)  # показывать по 10 записей на странице.
+
+    page_number = request.GET.get('page')  # переменная в URL с номером запрошенной страницы
+    page = paginator.get_page(page_number)  # получить записи с нужным смещением
+    return render(
+        request,
+        'index.html',
+        {'page': page, 'paginator': paginator}
+       )
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
