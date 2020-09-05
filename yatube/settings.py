4b12a6258b71c69a53610165c934888193ca3632
Ирина Бобина <bobin.ir495@gmail.com>
2020-09-05 23:59:44 +0300

    fb b

--- yatube/settings.py
+++ yatube/settings.py
@@ -38,6 +38,8 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'users',
     'posts',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
