81408728d7bf5ed20e640c84f62b15fabae174fd
Ирина Бобина <bobin.ir495@gmail.com>
2020-09-07 01:09:13 +0300

    hkjm

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,14 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("group/<slug:slug>/", views.group_posts, name="group"),
-    path("new/", views.new_post, name="new_post")
+    path("new/", views.new_post, name="new_post"),
+        # Профайл пользователя
+    path('<str:username>/', views.profile, name='profile'),
+        # Просмотр записи
+    path('<str:username>/<int:post_id>/', views.post_view, name='post'),
+    path(
+        '<str:username>/<int:post_id>/edit/', 
+        views.post_edit, 
+        name='post_edit'
+        ),
 ]
