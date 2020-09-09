df1f066dbe2fff653e6705eee5786b318930289a
Ирина Бобина <bobin.ir495@gmail.com>
2020-09-09 15:58:14 +0300

    gnhgng

--- posts/views.py
+++ posts/views.py
@@ -39,3 +39,31 @@ def new_post(request):
             return redirect('index')
         return render(request, 'index.html', {'form': form}) 
     return render(request, 'new.html', {'form': form})
+
+@login_required
+def profile(request, username):
+    profile = get_object_or_404(get_user_model(), username=username)
+    post_list = profile.posts.order_by('-pub_date').all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    context = {
+        'profile': profile,
+        'page': page,
+        'paginator': paginator,
+        'post_list': post_list
+    }
+        return render(request, 'profile.html', context)
+ 
+ 
+def post_view(request, username, post_id):
+        # тут тело функции
+        return render(request, 'post.html', {})
+
+
+def post_edit(request, username, post_id):
+        # тут тело функции. Не забудьте проверить, 
+        # что текущий пользователь — это автор записи.
+        # В качестве шаблона страницы редактирования укажите шаблон создания новой записи
+        # который вы создали раньше (вы могли назвать шаблон иначе)
+        return render(request, 'post_new.html', {})
